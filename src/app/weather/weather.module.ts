a34b4c8bf8faf5c3f53aa2f15510083a7611d572
Md Murad Hossain <mdmuradhossain33@gmail.com>
2021-09-24 11:02:46 +0600

    forecast component added

--- src/app/weather/weather.module.ts
+++ src/app/weather/weather.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ForecastComponent } from './forecast/forecast.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ForecastComponent],
   imports: [
     CommonModule
   ]
