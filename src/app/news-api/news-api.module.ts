91cd117e8021be71df571ab03c539c71a442d6c6
Md Murad Hossain <mdmuradhossain33@gmail.com>
2021-09-27 21:22:59 +0600

    NewsArticleList component added

--- src/app/news-api/news-api.module.ts
+++ src/app/news-api/news-api.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NewsArticleListComponent } from './news-article-list/news-article-list.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [NewsArticleListComponent],
   imports: [CommonModule],
   exports: [],
 })
---
+++ src/app/news-api/news-article-list/news-article-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-news-article-list',
+  templateUrl: './news-article-list.component.html',
+  styleUrls: ['./news-article-list.component.css']
+})
+export class NewsArticleListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
